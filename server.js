d51a3bdd28714ec5c13f10cde2da0ccc6fbe006b
chanki <alscksrl2010@gmail.com>
2020-08-20 15:21:59 +0900

    Reformat project structure
    프로젝트의 각 컴포넌트별로 디렉토를 만들어 분리하였고, 이에 따라 server.js 수정

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
-require('dotenv').config();
-const router = require('./route.js');
+const actionRouter = require('./router/actionRouter');
+const boilerPlateRouter = require('./router/boilerPlateRouter');
 
 //request의 body를 json으로 받아들인다.
 app.use(bodyParser.json({limit : '50mb'}));
 
 //라우터 적용
-app.use('/',router);
+app.use('/',actionRouter);
+app.use('/',boilerPlateRouter);
 
 const listener = app.listen(process.env.PORT, function() {
     console.log('Your app is listening on port ' + listener.address().port);
